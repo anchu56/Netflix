b1fcf8585a2181dce9fbbb2e5d20582df2b85b4a
Ansaf Muhammed <anchu56@gmail.com>
2022-01-22 14:29:45 +0530

    Created the Frontend of Netflix withour calling API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import NavBar from './Components/Navbar/NavBar';
+import './App.css'
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
 
 function App() {
-  return <div>
-    
-  </div>;
+  return (
+     <div className='App'>
+        <NavBar/>
+        <Banner/>
+        <RowPost/>
+     </div>
+  );
 }
 
 export default App;
